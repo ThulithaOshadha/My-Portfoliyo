32bac0b4677b499236304af5adc10f95244f1324
ThulithaOshadha <thulithaoshadha205@gmail.com>
2022-10-12 10:23:00 +0530

    load item & customer details in order page

--- assingments/JS/SPA/controllers/orderPageControllers.js
+++ assingments/JS/SPA/controllers/orderPageControllers.js
@@ -1,7 +1,55 @@
-function loadCusId(){
-    $('#inputCusID').empty();
-    for (let item of itemArray) {
-        $("#inputCusID").append('<option>${item.id}</option>');
+$("#inputOrderID").focus();
+
+function loadCusId() {
+    $("#inputCusID").empty();
+    for (let cus of customerArray) {
+        $("#inputCusID").append(`<option>${cus.id}</option>`);
     }
 
 }
+
+function loadCusDetails(){
+    let id = $("#inputCusID").val();
+    if (id!=null){
+        let customerDet = searchCustomer(id);
+        if (customerDet!=null){
+            $("#inputCusName").val(customerDet.name);
+            $("#inputAddress").val(customerDet.address);
+            $("#inputCusSalary").val(customerDet.salary);
+        }
+    }
+}
+
+
+$("#inputCusID").click(function (){
+    loadCusDetails();
+});
+
+
+
+
+function loadItemId() {
+    $("#inputItemID").empty();
+    for (let item of itemArray) {
+        $("#inputItemID").append(`<option>${item.id}</option>`);
+    }
+
+}
+
+
+$("#inputItemID").click(function (){
+    loadItemDetails();
+});
+
+function loadItemDetails(){
+    let id = $("#inputItemID").val();
+    if (id!=null){
+        let itemDet = searchItem(id);
+        if (itemDet!=null){
+            $("#inputItemName").val(itemDet.name);
+            $("#inputPrice").val(itemDet.price);
+            $("#inputQty").val(itemDet.qty);
+        }
+    }
+}
+
